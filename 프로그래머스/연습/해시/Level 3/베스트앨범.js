8f49132bf1d66f54d8976ce3da4fd663cee7bf0e
HIKARI\Hikari <kmuu011@naver.com>
2022-04-21 22:30:05 +0900

    해시 레벨 3 베스트 앨범 다른사람의 코드 참고

--- 프로그래머스/연습/해시/Level 3/베스트앨범.js
+++ 프로그래머스/연습/해시/Level 3/베스트앨범.js
@@ -84,3 +84,27 @@ function solution(genres, plays) {
 }
 
 console.log(solution(genres, plays));
+
+
+//다른 사람의 풀이 sort와 filter를 잘 이용함 참고가 되기 좋은듯.
+function solution1(genres, plays) {
+    var dic = {};
+    genres.forEach((t,i)=> {
+        dic[t] = dic[t] ?  dic[t] + plays[i] :plays[i];
+    });
+
+    var dupDic = {};
+    return genres
+        .map((t,i)=> ({genre : t, count:plays[i] , index:i}))
+        .sort((a,b)=>{
+            if(a.genre !== b.genre) return dic[b.genre] - dic[a.genre];
+            if(a.count !== b.count) return b.count - a.count;
+            return a.index - b.index;
+        })
+        .filter(t=>  {
+            if(dupDic[t.genre] >= 2) return false;
+            dupDic[t.genre] = dupDic[t.genre] ? dupDic[t.genre]+ 1 : 1;
+            return true;
+        })
+        .map(t=> t.index);
+}
