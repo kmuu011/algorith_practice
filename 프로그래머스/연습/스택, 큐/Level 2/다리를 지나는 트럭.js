d9dc27321c00089abe47a421063c7ae9d9085dae
HIKARI\Hikari <kmuu011@naver.com>
2022-04-25 22:08:43 +0900

    필요없는 로그 제거

--- 프로그래머스/연습/스택, 큐/Level 2/다리를 지나는 트럭.js
+++ 프로그래머스/연습/스택, 큐/Level 2/다리를 지나는 트럭.js
@@ -101,7 +101,6 @@ function solution1(bridge_length, weight, truck_weights) {
 
     // 대기 트럭, 다리를 건너는 트럭이 모두 0일 때 까지 다음 루프 반복
     while (qu.length > 0 || truck_weights.length > 0) {
-        console.log('test');
         // 1. 현재 시간이, 큐 맨 앞의 차의 '나갈 시간'과 같다면 내보내주고,
         //    다리 위 트럭 무게 합에서 빼준다.
         if (qu[0][1] === time) weightOnBridge -= qu.shift()[0];
